4bc046239c60860ecd007bbaceb53154092c7df7
gonzalo olivares <g.olivaresca@gmail.com>
2024-05-25 10:57:50 -0400

    Configuración DB settings.py

--- resena_libros/resena_libros/settings.py
+++ resena_libros/resena_libros/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'resena_libros.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "resena_libross",
+        "USER": "golivares",
+        "PASSWORD": "mildred1000",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
